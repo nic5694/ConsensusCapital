b320ce70a0a9e1e615362041537fc98fc1c4dd0d
dylanbrass <dylan.brassard@outlook.com>
2026-01-17 23:29:51 -0500

    fix: add cors

--- SpringBoot/src/main/java/consensus/api/com/springboot/configuration/ApplicationConfig.java
+++ SpringBoot/src/main/java/consensus/api/com/springboot/configuration/ApplicationConfig.java
@@ -1,12 +1,14 @@
 package consensus.api.com.springboot.configuration;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig implements WebMvcConfigurer {
@@ -15,6 +17,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
+        log.debug("Configuring CORS with allowed origin: {}", applicationProps.getClientOriginUrl());
         registry.addMapping("/**")
                 .allowedOrigins(applicationProps.getClientOriginUrl())
                 .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE)
