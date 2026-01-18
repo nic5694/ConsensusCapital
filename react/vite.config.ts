121da5b4735364e1df3ed71916a891e384ebe9c9
nic5694 <nicholasmartoccia04@icloud.com>
2026-01-17 21:16:13 -0500

    fixed the env issue

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'petclinic-frontend',
+        shared: ['react', 'react-dom'],
+      }),
+    ],
+    resolve: {
+      alias: {
+         '@/': new URL('./src/', import.meta.url).pathname,
+      },
+    },
+    envDir: 'src/environments/',
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      assestsDir: 'src/assets',
+    },
+  };
+});
