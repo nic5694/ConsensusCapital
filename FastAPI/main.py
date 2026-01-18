08d3b9f0d28da6c8f573c5580d8a840ad52b3b24
Barry Yu <77864928+realBarry123@users.noreply.github.com>
2026-01-18 03:18:51 -0500

    Expose long business summary

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,17 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
+import yfinance as yf
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/summary/{ticker}")
+async def get_long_business_summary(ticker_symbol):
+    ticker = yf.Ticker(ticker_symbol)
+    return ticker.get_info().get("longBusinessSummary")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=4242)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
